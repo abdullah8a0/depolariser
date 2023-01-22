cb6fceed61eb7fbc6a75f1be8329a025d20985ca
Ada <aerus@mit.edu>
2023-01-22 15:54:12 -0500

    add more info to main page (before login)

--- client/src/components/pages/Main.tsx
+++ client/src/components/pages/Main.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  GoogleOAuthProvider,
-  GoogleLogin,
-  googleLogout,
-  CredentialResponse,
-} from "@react-oauth/google";
+import { GoogleOAuthProvider, GoogleLogin, googleLogout, CredentialResponse } from "@react-oauth/google";
 
 import "./Main.css";
 import { Link, RouteComponentProps } from "@reach/router";
@@ -21,6 +16,7 @@ const Main = (props: Props) => {
   const { handleLogin, handleLogout } = props;
 
   // button redirects to /test
+  // main page seen when not logged on
   // the test button is shown when the user is logged in
   return (
     <>
@@ -37,7 +33,7 @@ const Main = (props: Props) => {
         ) : (
           <GoogleLogin onSuccess={handleLogin} onError={() => console.log("Error Logging in")} />
         )}
-        <h1>This is the Main Page</h1>
+
         {props.userId ? (
           <>
             <p> Take the test</p>
@@ -46,7 +42,32 @@ const Main = (props: Props) => {
             </Link>
           </>
         ) : (
-          <></>
+          <>
+            <head>
+              <title>Depolarizer</title>
+            </head>
+            <body>
+              <h1>Depolarizer</h1>
+              <section>
+                <h2>Mission</h2>
+                <h3>
+                  Our mission is to assess your political views through a test and provide a platform for you to access news sources that present
+                  opposing viewpoints, fostering critical thinking and informed discourse.
+                </h3>
+              </section>
+              <section>
+                <h3>Learn More</h3>
+                <Link to="/learnmore">
+                  <button>Learn More</button>
+                </Link>
+
+                <h3>Take Test</h3>
+                <Link to="/test">
+                  <button>Test</button>
+                </Link>
+              </section>
+            </body>
+          </>
         )}
       </GoogleOAuthProvider>
     </>
